d4e11533bd2f3c679eb0bfe5ba7570773df24b5c
Frode <frode.aa@gmail.com>
2011-03-06 17:41:29 +0100

    Implementation of HttpService, no support for basic authentication.

--- src/main/java/tfdhs/core/HttpURLConnectionHttpService.java
+++ src/main/java/tfdhs/core/HttpURLConnectionHttpService.java
@@ -1,13 +1,21 @@
 package tfdhs.core;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import tfdhs.api.Builder;
 import tfdhs.api.HttpBuilder;
 import tfdhs.api.HttpRequest;
 import tfdhs.api.HttpResponse;
@@ -36,7 +44,78 @@ public class HttpURLConnectionHttpService implements HttpService {
      */
     @Override
     public HttpResponse sendHttpRequest(HttpRequest request) {
-	return null;
+
+	HttpURLConnection connection = null;
+	int status = -1;
+	String message = null;
+	String response = null;
+	Map<String, String> responseHeaders = Collections.emptyMap();
+	try {
+	    HttpURLConnection.setFollowRedirects(request.isFollowRedirects());
+	    connection = newConnection(request);
+	    prepareConnection(connection, request);
+
+	    status = readStatus(connection);
+	    message = readMessage(connection);
+	    response = readResponse(connection);
+	    responseHeaders = readHeaders(connection);
+	} catch (IOException e) {
+	    //
+	} finally {
+	    if (connection != null) {
+		connection.disconnect();
+	    }
+	}
+
+	return asHttpResponse(status, message, response, responseHeaders);
+    }
+
+    /**
+     * Setup the connection with the properties from the request.
+     * 
+     * @param connection
+     *            the connection to setup.
+     * @param request
+     *            the request to set on connection.
+     * @throws IOException
+     *             if any I/O.
+     */
+    protected void prepareConnection(HttpURLConnection connection,
+	    HttpRequest request) throws IOException {
+
+	connection.setRequestMethod(request.getMethod().name());
+
+	String body = request.getBody();
+	if (body != null) {
+	    connection.setRequestProperty("Content-Length",
+		    "" + Integer.toString(body.getBytes().length));
+	    connection.setDoOutput(true);
+	    write(request.getBody(), connection.getOutputStream());
+	}
+    }
+
+    /**
+     * Create a HttpResponse from parameters.
+     * 
+     * @param status
+     *            the HTTP status code.
+     * @param message
+     *            the HTTP status message.
+     * @param response
+     *            the HTTP response content body.
+     * @param responseHeaders
+     *            the HTTP header fields.
+     * @return new HttpResponse craeted from parameters.
+     */
+    protected HttpResponse asHttpResponse(int status, String message,
+	    String response, Map<String, String> responseHeaders) {
+
+	Builder.Response<HttpResponse> responseBuilder = builder
+		.newResponse(status).message(message)
+		.body(response == null ? "" : response)
+		.headers(responseHeaders);
+
+	return responseBuilder.build();
     }
 
     /**
@@ -67,6 +146,36 @@ public class HttpURLConnectionHttpService implements HttpService {
 	return new URL(request.getUrl());
     }
 
+    protected static int readStatus(HttpURLConnection connection)
+	    throws IOException {
+	return connection.getResponseCode();
+    }
+
+    protected static String readMessage(HttpURLConnection connection)
+	    throws IOException {
+	return connection.getResponseMessage();
+    }
+
+    protected static String readResponse(HttpURLConnection connection)
+	    throws IOException {
+	InputStream is = connection.getInputStream();
+	return readInput(is);
+    }
+
+    protected static Map<String, String> readHeaders(
+	    HttpURLConnection connection) {
+
+	final Map<String, List<String>> responseHeaders = connection
+		.getHeaderFields();
+
+	final Map<String, String> headers = new HashMap<String, String>();
+	for (Entry<String, List<String>> header : responseHeaders.entrySet()) {
+	    headers.put(header.getKey(), flattenList(header.getValue()));
+	}
+
+	return headers;
+    }
+
     /**
      * Consumes an input stream.
      * 
@@ -78,6 +187,10 @@ public class HttpURLConnectionHttpService implements HttpService {
      */
     protected static String readInput(InputStream is) throws IOException {
 
+	if (is == null) {
+	    return "";
+	}
+
 	BufferedReader rd = new BufferedReader(new InputStreamReader(is));
 	StringBuffer dataRead = new StringBuffer();
 
@@ -87,6 +200,31 @@ public class HttpURLConnectionHttpService implements HttpService {
 	}
 
 	return dataRead.toString();
+    }
+
+    protected static void write(String text, OutputStream out)
+	    throws IOException {
+
+	DataOutputStream wr = null;
+	try {
+	    wr = new DataOutputStream(out);
+	    wr.writeBytes(text == null ? "" : text);
+	    wr.flush();
+	} catch (IOException e) {
+	} finally {
+	    wr.close();
+	}
+
+    }
+
+    protected static String flattenList(List<String> values) {
+	StringBuilder builder = new StringBuilder();
+	for (String value : values) {
+	    builder.append(value);
+	    builder.append(' ');
+	}
+
+	return builder.toString().trim();
     }
 
 }
