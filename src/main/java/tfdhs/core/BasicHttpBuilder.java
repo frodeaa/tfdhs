4470b49aabbf665aa6c617db946ba1c0e2ed7c0e
Frode <frode.aa@gmail.com>
2011-03-06 17:59:18 +0100

    Implement builder

---
+++ src/main/java/tfdhs/core/BasicHttpBuilder.java
@@ -0,0 +1,128 @@
+package tfdhs.core;
+
+import java.util.Collections;
+import java.util.Map;
+
+import tfdhs.api.Builder;
+import tfdhs.api.HttpBuilder;
+import tfdhs.api.HttpMethod;
+import tfdhs.api.HttpRequest;
+import tfdhs.api.HttpResponse;
+import tfdhs.api.HttpService;
+
+/**
+ * Default {@link HttpBuilder} implementation.
+ * 
+ * @author frode
+ * 
+ */
+public class BasicHttpBuilder implements HttpBuilder {
+
+    /**
+     * {@inheritDoc}
+     */
+    public HttpService newHttpService() {
+	return new HttpURLConnectionHttpService(this);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Builder.Request<HttpRequest> newRequest(String url, HttpMethod method) {
+	return new HttpRequestBuilder(url, method);
+    }
+
+    private class HttpRequestBuilder implements Builder.Request<HttpRequest> {
+
+	String url;
+	HttpMethod method;
+	Map<String, String> headers = Collections.emptyMap();
+	String body = null;
+	boolean followRedirects = false;
+
+	HttpRequestBuilder(String url, HttpMethod method) {
+	    this.url = url;
+	    this.method = method;
+	}
+
+	@Override
+	public tfdhs.api.Builder.Request<HttpRequest> url(String url) {
+	    this.url = url;
+	    return this;
+	}
+
+	public tfdhs.api.Builder.Request<HttpRequest> followRedirects(
+		boolean follow) {
+	    this.followRedirects = follow;
+	    return this;
+	}
+
+	public tfdhs.api.Builder.Request<HttpRequest> body(String body) {
+	    this.body = body;
+	    return this;
+
+	}
+
+	public tfdhs.api.Builder.Request<HttpRequest> headers(
+		Map<String, String> headers) {
+	    this.headers = headers;
+	    return this;
+	}
+
+	@Override
+	public HttpRequest build() {
+	    return new tfdhs.core.ImmutableRequest(url, method, headers, body,
+		    followRedirects);
+	}
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Builder.Response<HttpResponse> newResponse(int status) {
+	return new HttpResponseBuilder(status);
+    }
+
+    private class HttpResponseBuilder implements Builder.Response<HttpResponse> {
+
+	private int statusCode;
+	private String message = null;
+	private Map<String, String> headers = Collections.emptyMap();
+	private String body;
+
+	HttpResponseBuilder(int status) {
+	    this.statusCode = status;
+	}
+
+	public tfdhs.api.Builder.Response<HttpResponse> message(String message) {
+	    this.message = message;
+	    return this;
+	}
+
+	public tfdhs.api.Builder.Response<HttpResponse> headers(
+		Map<String, String> headers) {
+	    this.headers = headers;
+	    return this;
+	}
+
+	public tfdhs.api.Builder.Response<HttpResponse> body(String body) {
+	    this.body = body;
+	    return this;
+	}
+
+	@Override
+	public tfdhs.api.Builder.Response<HttpResponse> status(int statusCode) {
+	    this.statusCode = statusCode;
+	    return this;
+	}
+
+	@Override
+	public HttpResponse build() {
+	    return new tfdhs.core.ImmutableResponse(statusCode, message,
+		    headers, body);
+	}
+
+    }
+
+}
---
+++ src/test/java/tfdhs/core/BasicHttpBuildersTest.java
@@ -0,0 +1,106 @@
+package tfdhs.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import tfdhs.api.Builder;
+import tfdhs.api.HttpMethod;
+import tfdhs.api.HttpRequest;
+import tfdhs.api.HttpResponse;
+
+public class BasicHttpBuildersTest {
+
+    private BasicHttpBuilder builder;
+
+    @Before
+    public void setUp() {
+	builder = new BasicHttpBuilder();
+    }
+
+    @Test
+    public void testNewHttpService() {
+
+	assertNotNull("Service null", builder.newHttpService());
+
+    }
+
+    @Test
+    public void testNewRequest() {
+
+	Builder.Request<HttpRequest> reqBuilder = builder.newRequest("someurl",
+		HttpMethod.POST);
+
+	assertNotNull("Request builder null", reqBuilder);
+
+    }
+
+    @Test
+    public void testNewResponse() {
+
+	Builder.Response<HttpResponse> reqBuilder = builder.newResponse(2030);
+
+	assertNotNull("Response builder null", reqBuilder);
+
+	assertEquals(2030, reqBuilder.build().getStatus());
+
+    }
+
+    @Test
+    public void testNewResponseBuilder() {
+
+	Builder.Response<HttpResponse> reqBuilder = builder.newResponse(-1);
+
+	Map<String, String> headers = Collections.singletonMap("time", "now");
+	HttpResponse response = reqBuilder.body("body").headers(headers)
+		.message("message").status(220).build();
+
+	assertNotNull("Response null", response);
+	assertEquals(220, response.getStatus());
+	assertEquals("body", response.getBody());
+	assertEquals("message", response.getMessage());
+	assertEquals(headers, response.getHeaders());
+
+	assertNotNull(response.toString());
+
+    }
+
+    @Test
+    public void testNewRequeset() {
+
+	Builder.Request<HttpRequest> reqBuilder = builder.newRequest("someurl",
+		HttpMethod.HEAD);
+
+	assertNotNull("Response builder null", reqBuilder);
+
+	assertEquals("someurl", reqBuilder.build().getUrl());
+	assertEquals(HttpMethod.HEAD, reqBuilder.build().getMethod());
+
+    }
+
+    @Test
+    public void testNewRequestBuilder() {
+
+	Builder.Request<HttpRequest> reqBuilder = builder.newRequest("someurl",
+		HttpMethod.HEAD);
+
+	Map<String, String> headers = Collections.singletonMap("time", "now");
+	HttpRequest request = reqBuilder.body("body").headers(headers)
+		.followRedirects(true).url("someUrl").build();
+
+	assertNotNull("Response null", request);
+	assertEquals("body", request.getBody());
+	assertEquals(headers, request.getHeaders());
+	assertTrue(request.isFollowRedirects());
+
+	assertNotNull(request.toString());
+
+    }
+
+}
