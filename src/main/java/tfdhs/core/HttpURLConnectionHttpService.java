b8765a3ef1f08b74aecaa985abcadb15b7dd6883
Frode <frode.aa@gmail.com>
2011-03-06 14:52:41 +0100

    helper methods for creating URL and connection.

--- src/main/java/tfdhs/core/HttpURLConnectionHttpService.java
+++ src/main/java/tfdhs/core/HttpURLConnectionHttpService.java
@@ -1,7 +1,11 @@
 package tfdhs.core;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import tfdhs.api.HttpBuilder;
 import tfdhs.api.HttpRequest;
 import tfdhs.api.HttpResponse;
 import tfdhs.api.HttpService;
@@ -14,9 +18,50 @@ import tfdhs.api.HttpService;
  */
 public class HttpURLConnectionHttpService implements HttpService {
 
+    private final HttpBuilder builder;
+
+    /**
+     * @param builder
+     *            the builder to use when building HttpResponse.
+     */
+    public HttpURLConnectionHttpService(HttpBuilder builder) {
+	this.builder = builder;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public HttpResponse sendHttpRequest(HttpRequest request) {
 	return null;
+    }
+
+    /**
+     * Opens a new connection to the request URL.
+     * 
+     * @param request
+     *            the request to create connection to.
+     * @return new HttpURLConnection.
+     * @throws IOException
+     *             if any I/O errors.
+     */
+    protected HttpURLConnection newConnection(HttpRequest request)
+	    throws IOException {
+	URL url = newUrl(request);
+	return (HttpURLConnection) url.openConnection();
+    }
+
+    /**
+     * Create a new URL from request.
+     * 
+     * @param request
+     *            the request to create URL from..
+     * @return new request URL..
+     * @throws MalformedURLException
+     *             if the request URL specify a uknown protocol.
+     */
+    protected URL newUrl(HttpRequest request) throws MalformedURLException {
+	return new URL(request.getUrl());
     }
 
 }
---
+++ src/test/java/tfdhs/core/HttpURLConnectionHttpServiceTest.java
@@ -0,0 +1,77 @@
+package tfdhs.core;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import tfdhs.api.HttpRequest;
+
+public class HttpURLConnectionHttpServiceTest {
+
+    private HttpURLConnectionHttpService service;
+
+    @Before
+    public void setUp() {
+	service = new HttpURLConnectionHttpService(null);
+    }
+
+    @Test
+    public void testNewConnection() throws IOException {
+
+	HttpRequest mockRequest = mock(HttpRequest.class);
+
+	when(mockRequest.getUrl()).thenReturn("http://localhost");
+
+	HttpURLConnection connection = null;
+	try {
+	    connection = service.newConnection(mockRequest);
+	    assertNotNull("Connection was null", connection);
+	} catch (IOException e) {
+	    fail("Unable to create new connection, got\n" + e);
+	} finally {
+	    if (connection != null) {
+		connection.disconnect();
+	    }
+	}
+
+    }
+
+    @Test(expected = MalformedURLException.class)
+    public void testNewConnectionFailsUrl() throws IOException {
+
+	HttpRequest mockRequest = mock(HttpRequest.class);
+
+	service.newConnection(mockRequest);
+
+    }
+
+    @Test(expected = MalformedURLException.class)
+    public void testNewUrlFailMalformedUrl() throws MalformedURLException {
+
+	HttpRequest mockRequest = mock(HttpRequest.class);
+
+	service.newUrl(mockRequest);
+
+    }
+
+    @Test
+    public void testNewUrl() throws MalformedURLException {
+
+	HttpRequest mockRequest = mock(HttpRequest.class);
+
+	when(mockRequest.getUrl()).thenReturn("http://localhost");
+
+	URL url = service.newUrl(mockRequest);
+
+	assertNotNull("URL null", url);
+	assertEquals("URL host", "localhost", url.getHost());
+
+    }
+
+}
