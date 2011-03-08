0e13dfa6696f4bb330676da14420b50798f99f26
Frode <frode.aa@gmail.com>
2011-03-09 00:35:23 +0100

    use the Authenticator implementation., user will now be asked for credentials if the request requires it.

--- src/main/java/tfdhs/HttpClient.java
+++ src/main/java/tfdhs/HttpClient.java
@@ -1,5 +1,7 @@
 package tfdhs;
 
+import java.net.Authenticator;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -7,8 +9,9 @@ import javax.swing.SwingUtilities;
 import tfdhs.api.HttpBuilder;
 import tfdhs.core.BasicHttpBuilder;
 import tfdhs.core.ClientController;
-import tfdhs.core.ClientWindowController;
 import tfdhs.core.ClientModel;
+import tfdhs.core.ClientWindowController;
+import tfdhs.core.ui.DialogAuthenticatorResolver;
 import tfdhs.core.ui.HttpClientWindow;
 
 public class HttpClient extends JFrame {
@@ -22,19 +25,19 @@ public class HttpClient extends JFrame {
     }
 
     public static void main(String[] args) {
-
 	final HttpClientWindow window = new HttpClientWindow();
 	final ClientModel model = new ClientModel();
-	final ClientController controller = new ClientWindowController(model, window,
-		builder);
+	final ClientController controller = new ClientWindowController(model,
+		window, builder);
 
 	SwingUtilities.invokeLater(new Runnable() {
 
 	    @Override
 	    public void run() {
 		JFrame frame = new HttpClient(window.build());
+		Authenticator
+			.setDefault(new DialogAuthenticatorResolver(frame));
 		window.bind(controller);
-
 		frame.setVisible(true);
 
 	    }
