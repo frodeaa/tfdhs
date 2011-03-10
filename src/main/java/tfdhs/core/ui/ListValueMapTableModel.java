7936793cd5b797f38ec7f19cab11819547331ecd
Frode <frode.aa@gmail.com>
2011-03-10 16:54:29 +0100

    adding new rows, removing row and getting row count

--- src/main/java/tfdhs/core/ui/ListValueMapTableModel.java
+++ src/main/java/tfdhs/core/ui/ListValueMapTableModel.java
@@ -1,5 +1,76 @@
 package tfdhs.core.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.table.DefaultTableModel;
+
 public class ListValueMapTableModel {
+
+    private final DefaultTableModel model;
+
+    /**
+     * Create a new Header Field Table model with two columns, the name of the
+     * header and its value.
+     */
+    public ListValueMapTableModel() {
+	model = new DefaultTableModel(new String[] { "Header Name",
+		"Header Value" }, 0);
+    }
+
+    /**
+     * Update the model to contain the (key,listItem) found in the map. Adds a
+     * row for each element found in the list of key values.
+     * 
+     * @param data
+     *            the data to update the table model with.
+     */
+    public void updateModel(Map<String, List<String>> data) {
+	model.setRowCount(0);
+	for (Entry<String, List<String>> header : data.entrySet()) {
+	    for (String value : header.getValue()) {
+		model.addRow(new Object[] { header.getKey(), value });
+	    }
+	}
+    }
+
+    /**
+     * Insert (key,value) to map, will create a new key->list entry if the map
+     * does not contain the key. The list that the key maps to will contain
+     * duplicates if insert is called multiple times with same (key,value) pair.
+     * 
+     * @param key
+     *            the key to add to the map.
+     * @param value
+     *            the value to add to the list that the key maps to.
+     * @param map
+     *            the map to insert (key,value) into.
+     */
+    protected static void insert(String key, String value,
+	    Map<String, List<String>> map) {
+	if (!map.containsKey(key)) {
+	    map.put(key, new ArrayList<String>());
+	}
+	map.get(key).add(value);
+    }
+
+    /**
+     * Removes the row at row from the model. Notification of the row being
+     * removed will be sent to all the listeners.
+     * 
+     * @param row
+     *            the row to remove.
+     * @throws ArrayIndexOutOfBoundsException
+     *             if the row was invalid
+     */
+    public void removeRow(int row) {
+	model.removeRow(row);
+    }
+
+    public int getRowCount() {
+	return model.getRowCount();
+    }
 
 }
---
+++ src/test/java/tfdhs/core/ui/ListValueMapTableModelTest.java
@@ -0,0 +1,68 @@
+package tfdhs.core.ui;
+
+import static org.junit.Assert.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test {@link ListValueMapTableModel}.
+ * 
+ * @author frode
+ * 
+ */
+public class ListValueMapTableModelTest {
+
+    private ListValueMapTableModel model;
+
+    @Before
+    public void setUp() {
+	model = new ListValueMapTableModel();
+    }
+
+    @Test
+    public void testUpdateModel() {
+
+	Map<String, List<String>> data = new HashMap<String, List<String>>();
+	data.put("Header1", Collections.nCopies(3, "Value1"));
+	data.put("Header2", Collections.nCopies(3, "Value2"));
+
+	model.updateModel(data);
+
+	assertEquals("Model row count", 6, model.getRowCount());
+
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testRemoveRowWhenEmptyThrowsIndexOutOfBounds() {
+
+	assertEquals("Model should be empty", 0, model.getRowCount());
+
+	model.removeRow(0);
+
+    }
+
+    @Test
+    public void testRemoveRow() {
+
+	Map<String, List<String>> data = new HashMap<String, List<String>>();
+	data.put("Header", Collections.nCopies(3, "Value"));
+	model.updateModel(data);
+
+	assertEquals("Model row count", 3, model.getRowCount());
+
+	model.removeRow(0);
+	assertEquals("Model row count", 2, model.getRowCount());
+
+	model.removeRow(0);
+	assertEquals("Model row count", 1, model.getRowCount());
+
+	model.removeRow(0);
+	assertEquals("Model row count", 0, model.getRowCount());
+    }
+}
