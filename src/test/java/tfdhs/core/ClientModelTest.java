95a6db525de4f2e4ae4838ac77a2ce6c714cffd9
Frode <frode.aa@gmail.com>
2011-03-10 19:44:34 +0100

    test hashcode and equals and tostring

--- src/test/java/tfdhs/core/ClientModelTest.java
+++ src/test/java/tfdhs/core/ClientModelTest.java
@@ -1,6 +1,12 @@
 package tfdhs.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,7 +25,7 @@ public class ClientModelTest {
     @Test
     public void testNewDefaults() {
 
-//	assertNotNull("Header table model null", model.getHeaders());
+	assertNotNull("Header table model null", model.getHeaderFields());
 	assertEquals("Default method", HttpMethod.GET, model.getMethod());
 	assertNull("Url not null", model.getUrl());
 	assertNull("Body not null", model.getBody());
@@ -29,17 +35,134 @@ public class ClientModelTest {
     }
 
     @Test
-    public void testHeaderColumns() {
+    public void testHashCode() {
+	int hash = model.hashCode();
+	int newHash = model.hashCode();
+	assertTrue("HAshCode", hash == newHash);
 
-//	assertEquals("Colum 0 name", "Header Name", model.getHeaders()
-//		.getColumnName(0));
-//	assertEquals("Colum 1 name", "Header Value", model.getHeaders()
-//		.getColumnName(1));
-//
-//	assertEquals("Column class", String.class, model.getHeaders()
-//		.getColumnClass(0));
-//	assertEquals("Column class", String.class, model.getHeaders()
-//		.getColumnClass(1));
+	hash = model.hashCode();
+	model.setBody("body");
+	newHash = model.hashCode();
+	assertFalse("HAshCode", hash == newHash);
+
+	hash = model.hashCode();
+	model.setBodySet(true);
+	newHash = model.hashCode();
+	assertFalse("HAshCode", hash == newHash);
+
+	hash = model.hashCode();
+	model.setFollowRedirects(true);
+	newHash = model.hashCode();
+	assertFalse("HAshCode", hash == newHash);
+
+	hash = model.hashCode();
+	model.getHeaderFields().put("value", new ArrayList<String>());
+	newHash = model.hashCode();
+	assertFalse("HAshCode", hash == newHash);
+
+	hash = model.hashCode();
+	model.setMethod(HttpMethod.POST);
+	newHash = model.hashCode();
+	assertFalse("HAshCode", hash == newHash);
+
+	hash = model.hashCode();
+	model.setUrl("someurl");
+	newHash = model.hashCode();
+	assertFalse("HAshCode", hash == newHash);
+
+    }
+
+    @Test
+    public void testHashCodeFieldsNull() {
+
+	int hash = model.hashCode();
+	model.setBody(null);
+	model.setBodySet(false);
+	model.setFollowRedirects(false);
+	model.setHeaderFields(null);
+	model.setMethod(null);
+	model.setUrl(null);
+	int newHash = model.hashCode();
+	assertFalse("HAshCode", hash == newHash);
+
+    }
+
+    @Test
+    public void testEquals() {
+
+	assertFalse(model.equals(null));
+	assertFalse(model.equals(""));
+	assertTrue(model.equals(model));
+
+	ClientModel other = new ClientModel();
+	assertTrue(model.equals(other));
+
+	model.setBody("");
+	other.setBody(null);
+	assertFalse(model.equals(other));
+
+	other.setBody("");
+	assertTrue(model.equals(other));
+
+	model.setBody(null);
+	assertFalse(model.equals(other));
+
+	other.setBody(null);
+	model.setBody(null);
+	assertTrue(model.equals(other));
+
+	model.setBodySet(true);
+	assertFalse(model.equals(other));
+
+	other.setBodySet(true);
+	assertTrue(model.equals(other));
+
+	model.setFollowRedirects(true);
+	assertFalse(model.equals(other));
+
+	other.setFollowRedirects(true);
+	assertTrue(model.equals(other));
+
+	model.getHeaderFields().put("value", new ArrayList<String>());
+	assertFalse(model.equals(other));
+
+	other.getHeaderFields().put("value", new ArrayList<String>());
+	assertTrue(model.equals(other));
+
+	model.setHeaderFields(null);
+	assertFalse(model.equals(other));
+
+	other.setHeaderFields(null);
+	assertTrue(model.equals(other));
+
+	model.setMethod(HttpMethod.POST);
+	assertFalse(model.equals(other));
+
+	other.setMethod(HttpMethod.POST);
+	assertTrue(model.equals(other));
+
+	model.setUrl("");
+	other.setUrl(null);
+	assertFalse(model.equals(other));
+
+	other.setUrl("");
+	assertTrue(model.equals(other));
+
+	model.setUrl(null);
+	assertFalse(model.equals(other));
+
+	other.setUrl(null);
+	model.setUrl(null);
+	assertTrue(model.equals(other));
+
+    }
+
+    @Test
+    public void toStringContainerUrl() {
+
+	model.setUrl("http://theurl");
+
+	assertTrue("Missing url", model.toString().contains("http://theurl"));
 
     }
 }
