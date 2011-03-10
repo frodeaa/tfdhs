8281933d311b7e9b18c887dcacd531cfd929be88
Frode <frode.aa@gmail.com>
2011-03-10 16:42:11 +0100

    initialize the headerFields field and override equals and hashCode.

--- src/main/java/tfdhs/core/ClientModel.java
+++ src/main/java/tfdhs/core/ClientModel.java
@@ -1,5 +1,7 @@
 package tfdhs.core;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,6 +31,7 @@ public class ClientModel {
 	setMethod(HttpMethod.GET);
 	setBody(null);
 	setBodySet(false);
+	setHeaderFields(new HashMap<String, List<String>>());
     }
 
     public String getUrl() {
@@ -77,6 +80,53 @@ public class ClientModel {
 
     public void setHeaderFields(Map<String, List<String>> headerFields) {
 	this.headerFields = headerFields;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((body == null) ? 0 : body.hashCode());
+	result = prime * result + (bodySet ? 1231 : 1237);
+	result = prime * result + (followRedirects ? 1231 : 1237);
+	result = prime * result
+		+ ((headerFields == null) ? 0 : headerFields.hashCode());
+	result = prime * result + ((method == null) ? 0 : method.hashCode());
+	result = prime * result + ((url == null) ? 0 : url.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	ClientModel other = (ClientModel) obj;
+	if (body == null) {
+	    if (other.body != null)
+		return false;
+	} else if (!body.equals(other.body))
+	    return false;
+	if (bodySet != other.bodySet)
+	    return false;
+	if (followRedirects != other.followRedirects)
+	    return false;
+	if (headerFields == null) {
+	    if (other.headerFields != null)
+		return false;
+	} else if (!headerFields.equals(other.headerFields))
+	    return false;
+	if (method != other.method)
+	    return false;
+	if (url == null) {
+	    if (other.url != null)
+		return false;
+	} else if (!url.equals(other.url))
+	    return false;
+	return true;
     }
 
     @Override
