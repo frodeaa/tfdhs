1f33af05f0fd039c16e98bb6df69e807dcaf1128
Frode <frode.aa@gmail.com>
2011-03-16 20:05:10 +0100

    default will be to ignore all certificate and hostname verification, not important when testing HTTP...

--- src/main/java/tfdhs/HttpClient.java
+++ src/main/java/tfdhs/HttpClient.java
@@ -1,7 +1,10 @@
 package tfdhs;
 
 import java.net.Authenticator;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -11,6 +14,8 @@ import tfdhs.core.BasicHttpBuilder;
 import tfdhs.core.ClientController;
 import tfdhs.core.ClientModel;
 import tfdhs.core.ClientWindowController;
+import tfdhs.core.HttpsHostnameVerifier;
+import tfdhs.core.TrustManager;
 import tfdhs.core.ui.DialogAuthenticatorResolver;
 import tfdhs.core.ui.HttpClientWindow;
 
@@ -18,13 +23,36 @@ public class HttpClient extends JFrame {
 
     private static HttpBuilder builder = new BasicHttpBuilder();
 
+    /**
+     * @param window
+     *            the root window panel that will be added to the frame.
+     */
     public HttpClient(JPanel window) {
 	add(window);
 	pack();
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public static void main(String[] args) {
+    /**
+     * Setup the HTTPClientService to ignore all Certificates.
+     * @throws KeyManagementException
+     * @throws NoSuchAlgorithmException
+     */
+    protected static void ignoreAllSecureCertificates()
+	    throws KeyManagementException, NoSuchAlgorithmException {
+
+	HttpsURLConnection.setDefaultSSLSocketFactory(TrustManager
+		.newSocketFactory(TrustManager.trustAllCerts));
+	HttpsURLConnection
+		.setDefaultHostnameVerifier(HttpsHostnameVerifier.allHostsValid);
+
+    }
+
+    public static void main(String[] args) throws KeyManagementException,
+	    NoSuchAlgorithmException {
+	
+	ignoreAllSecureCertificates();
+
 	final HttpClientWindow window = new HttpClientWindow();
 	final ClientModel model = new ClientModel();
 	final ClientController controller = new ClientWindowController(model,
